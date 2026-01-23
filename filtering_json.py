323ae3dcd7a9c0e184d8a9fb68e3098aaf9904d3
mirkocuchan <mirko.cuchan01@gmail.com>
2026-01-23 03:26:40 +0000

    dataset filtering, and weight of books calculations

--- filtering_json.py
+++ filtering_json.py
@@ -10,4 +10,16 @@ books_wo_duplicates = sorted_books.drop_duplicates(subset=['url'], keep='first')
 books_wo_duplicates.to_csv('books.csv', index=False, encoding='utf-8-sig')
 
 
+avg_rating_all_books = books_wo_duplicates['avg_rating'].mean()
+min_trustable_votes = books_wo_duplicates['num_ratings'].quantile(0.25)
+
+#q_books = books_wo_duplicates.copy().loc[books_wo_duplicates['num_ratings'] >= min_trustable_votes]
+#shape = q_books.shape
+
+#shape1 = books_wo_duplicates.shape
+#print(shape)
+#print(shape1)
+
+print(min_trustable_votes)
+#weight_of_rating = ((votes_of_book // votes_of_book + min_trustable_votes) * book_average_score) + (min_trustable_votes // (min_trustable_votes + votes_of_book) * avg_rating_all_books)
 
