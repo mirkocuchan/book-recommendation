c1c6d91cbc7bcbdf1792256a3be5e099cc8d033b
mirkocuchan <mirko.cuchan01@gmail.com>
2026-01-27 02:56:22 +0000

    working recommendator, need to polish some functions

--- filtering_json.py
+++ filtering_json.py
@@ -2,7 +2,7 @@ import pandas as pd
 import re
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
-
+from rapidfuzz import process, utils
 
 def clean_description(text):
     if not isinstance(text, str):
@@ -19,10 +19,8 @@ def clean_description(text):
 
 books = pd.read_json('dataset_4k.json')
 
-sorted_books = books.sort_values(by='num_ratings', ascending=False)
-
+books_wo_duplicates = books.sort_values(by='num_ratings', ascending=False).drop_duplicates(subset=['title', 'author'], keep='first').copy()
 #duplicados = sorted_books[sorted_books.duplicated(subset=['url'], keep=False)]
-books_wo_duplicates = sorted_books.drop_duplicates(subset=['title', 'author'], keep='first').copy()
 
 avg_rating_all_books = books_wo_duplicates['avg_rating'].mean()
 min_trustable_votes = books_wo_duplicates['num_ratings'].quantile(0.25)
@@ -45,11 +43,12 @@ books_wo_duplicates['weighted_score'] = books_wo_duplicates.apply(
 )
 
 books_final = books_wo_duplicates.sort_values(by='weighted_score', ascending=False)
+books_final = books_final.reset_index(drop=True)
 
 books_final['description_clean'] = books_final['description'].apply(clean_description)
 
 tfidf = TfidfVectorizer(stop_words='english', min_df=5, max_df=0.8)
-books_final['description_clean'] = books_final['description_clean'].fillna('')
+#books_final['description_clean'] = books_final['description_clean'].fillna('')
 
 tfidf_matrix = tfidf.fit_transform(books_final['description_clean'])
 
@@ -59,11 +58,39 @@ tfidf_matrix.shape
 #this helps us get the similarities between the books, based on the words they use in the descriptions
 #cosine similarity, useful formula for this step, wikipedia page is very good
 cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+#with the title we can identify the index of a book
 
 indices = pd.Series(books_final.index, index=books_final['title']).drop_duplicates()
 
-books_final.to_csv('books_scored.csv', index=False, encoding='utf-8-sig')
+#books_final.to_csv('books_scored.csv', index=False, encoding='utf-8-sig')
 
+def get_recommendations(title, cosine_sim=cosine_sim):
+    try:
+        idx = indices[title]
 
+        sim_scores = list(enumerate(cosine_sim[idx]))
+
+        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+        sim_scores = sim_scores[1:11]
+
+        book_indices = [i[0] for i in sim_scores]
+
+        return books_final[['title', 'author', 'weighted_score']].iloc[book_indices].reset_index(drop=True)
+    except KeyError:
+        return f"Libro '{title}' no está el dataset."
+
+while True:
+    user_input = input("\nName of the book you enjoyed (or just write 'exit'): ")
+    
+    if user_input.lower() == 'exit':
+        break
+        
+    recomendaciones = get_recommendations(user_input)
+    
+    if isinstance(recomendaciones, str):
+        print(recomendaciones)
+    else:
+        print(f"\nIf you liked '{user_input}', you should try:\n")
+        print(recomendaciones)
 #weight_of_rating = ((votes_of_book // votes_of_book + min_trustable_votes) * book_average_score) + (min_trustable_votes // (min_trustable_votes + votes_of_book) * avg_rating_all_books)
 
