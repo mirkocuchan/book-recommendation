eaa71293e04b434728765f85f105a9d6d13e2f7d
mirkocuchan <mirko.cuchan01@gmail.com>
2026-06-11 23:16:50 +0000

    code cleaning, eliminating old ideas and redundancy

--- main.py
+++ main.py
@@ -3,42 +3,7 @@ from web_scrapping_listopia import scrape_goodreads_dataset, save_dataset, merge
 import json
 
 def main():
-    #print("Welcome! Describe the book you want.")
-    #print("Please, if you want to search by author start your search with 'Author: '")
-    #print("Please, if you want to search by genre start your search with 'Genre: '")
-    #print("CC, write whatever you want")
-
-
-    #user_input = (input(""))
-    #res = web_scrapping(user_input)
-    #print(res)
-#    genres = [
-#        "fantasy",
-#        "fiction",
-#        "non-fiction",
-#        "romance",
-#        "mystery",
-#        "classics",
-#        "horror",
-#        "science-fiction",
-#        "thriller",
-#        "poetry",
-#        "historical",
-#        "philosophy",
-#        "biography",
-#    ]
-
-#    all_books = []
-
-#    for g in genres:
-#        print(f"\n=== Scraping genre: {g} ===")
-#        books = scrape_goodreads_dataset(genre=g, num_books=50)
-#        all_books.extend(books)
-
-#    print(f"\nTotal books scraped: {len(all_books)}")
-
-#    with open("goodreads_multi_genres.json", "w", encoding="utf-8") as f:
-#        json.dump(all_books, f, ensure_ascii=False, indent=2)
+    
 
     scrape_goodreads_dataset("fiction", num_books=3000)
     scrape_goodreads_dataset("non-fiction", num_books=1000)
